6117b2b504040d778bdebdc751d5235615044e65
Sym Whysel <simone.whysel@gmail.com>
2024-09-11 19:49:45 -0400

    Set up pong game

--- Project_01_Pong/Assets/Ball.cs
+++ Project_01_Pong/Assets/Ball.cs
@@ -6,13 +6,13 @@ public class Ball : MonoBehaviour
 {
     public Rigidbody2D r2d;
     public Vector2 LocalVelocity;
+    public Vector3 StartPos;
+    public float Speed = 5.0f; 
 
     // Start is called before the first frame update
     void Start()
     {
-        r2d.GetComponent<Rigidbody2D>();
-        r2d.velocity = new Vector2(-3.0f, 1f);
-        LocalVelocity = r2d.velocity;
+        ResetBall();
     }
 
     // Update is called once per frame
@@ -34,4 +34,25 @@ public class Ball : MonoBehaviour
             //get info from collision ie normal, point of contact
             collision.GetContact(0).normal);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit Trigger");
+        
+        ResetBall();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision){
+
+    }
+
+    public void ResetBall()
+    {
+        transform.position = StartPos;
+        r2d.GetComponent<Rigidbody2D>();
+        r2d.velocity = new Vector2(Random.Range(-1f, 1f), (Random.Range(-1f, 1f)));
+        r2d.velocity.Normalize();
+        LocalVelocity = r2d.velocity * Speed;
+    }
+
 }
